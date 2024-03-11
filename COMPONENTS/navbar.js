d329ab98e980e282511f93505bc2af9644556111
Shane Stevens <shanestevensxk9@gmail.com>
2024-03-11 12:35:07 +0200

    update

--- COMPONENTS/navbar.js
+++ COMPONENTS/navbar.js
@@ -33,12 +33,12 @@ menu
 
 
 
-
 <!-- Projects -->
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  <div class="offcanvas-header py-5">
+    <button id="close_projects_btn" type="button" class="text-reset" data-bs-dismiss="offcanvas" aria-label="Close">
+    <i id="close_projects_icon" class="fa-solid fa-x"></i>
+    </button>
   </div>
   <div class="offcanvas-body">
     <div>
